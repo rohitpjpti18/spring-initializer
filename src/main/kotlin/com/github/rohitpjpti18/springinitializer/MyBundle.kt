3ddbdf97e1aef6bdcafd7309f2e3341dab511764
GitHub Action <action@github.com>
2023-10-14 14:53:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/rohitpjpti18/springinitializer/MyBundle.kt
+++ src/main/kotlin/com/github/rohitpjpti18/springinitializer/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.rohitpjpti18.springinitializer
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/rohitpjpti18/springinitializer/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/rohitpjpti18/springinitializer/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.rohitpjpti18.springinitializer.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
